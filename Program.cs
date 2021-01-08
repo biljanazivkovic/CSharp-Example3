6160e0996d393f880b985db3e9d328b21db03249
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:05:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool firstVariable = true;
+            bool secondVariable = false;
+            Console.WriteLine(secondVariable);
+            Console.WriteLine(firstVariable);
+
+
+            int x = 10;
+            int y = 9;
+            Console.WriteLine(x > y);
+
+            Console.WriteLine(10 > 9);
+        }
+    }
+}
